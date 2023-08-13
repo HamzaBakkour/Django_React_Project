131f1d5482b437cbc3a8c00dd672317e659e2624
HamzaBakkour <alexander.x4.hb@outlook.com>
2023-08-13 12:02:05 +0200

    init commit

---
+++ CoreRoot/urls.py
@@ -0,0 +1,36 @@
+#CoreRoot/urls
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, \
+                                        SpectacularRedocView, \
+                                        SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+        path('admin/',
+                admin.site.urls),
+
+        path('api/',
+                include(('core.routers', 'core'),
+                        namespace = "core-api")),
+
+        path("api/scheam/",
+                SpectacularAPIView.as_view(),
+                name = "schema"),
+
+        path("api/schema/redoc/",
+                SpectacularRedocView.as_view(url_name="schema"),
+                name="redoc",), 
+
+        path("api/schema/swagger-ui/",
+                SpectacularSwaggerView.as_view(url_name="schema"),
+                name="swagger-ui"), 
+
+]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
