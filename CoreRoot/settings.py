86c4058457662b04bc9f9854afb4b5ae178eddcf
HamzaBakkour <alexander.x4.hb@outlook.com>
2023-08-18 19:22:11 +0200

    minor fixes

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -179,11 +179,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    os.getenv("CORS_ALLOWED_ORIGINS", "").split(","),
-]
+CORS_ALLOWED_ORIGINS =  os.getenv("CORS_ALLOWED_ORIGINS", "").split(","),
+
 
 # Media files
 MEDIA_URL = '/media/'
