3fb990207afe508f60dcc46241c0494c5e606dda
HamzaBakkour <alexander.x4.hb@outlook.com>
2023-08-21 00:30:52 +0200

    m-c

--- social-media-app/src/helpers/axios.js
+++ social-media-app/src/helpers/axios.js
@@ -1,62 +1,60 @@
-//src/helprers/axios.js
 import axios from "axios";
 import createAuthRefreshInterceptor from "axios-auth-refresh";
-import { getAccessToken, getRefreshToken, getUser,}
-    from "../hooks/user.actions";
-
-
+import {
+  getAccessToken,
+  getRefreshToken,
+  getUser,
+} from "../hooks/user.actions";
 
 const axiosService = axios.create({
-    baseURL : "ec2-16-170-201-40.eu-north-1.compute.amazonaws.com",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: "ec2-16-170-201-40.eu-north-1.compute.amazonaws.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
- 
 
 axiosService.interceptors.request.use(async (config) => {
-    /**
+  /**
    * Retrieving the access and refresh tokens from the local storage
    */
-    config.headers.Authorization = `Bearer ${getAccessToken()}`;
-    return config;
+  config.headers.Authorization = `Bearer ${getAccessToken()}`;
+  return config;
 });
 
-
-
 axiosService.interceptors.response.use(
-    (res) => Promise.resolve(res),
-    (err) => Promise.reject(err)
+  (res) => Promise.resolve(res),
+  (err) => Promise.reject(err)
 );
 
-
-
 const refreshAuthLogic = async (failedRequest) => {
-    return axios
-        .post("/auth/refresh/",
-                { refresh: getRefreshToken(),},
-                { baseURL: "ec2-16-170-201-40.eu-north-1.compute.amazonaws.com",})
-
-        .then((resp) => {
-            const { access } = resp.data;
-            failedRequest.response.config.headers["Authorization"] =
-                "Bearer " + access;
-            localStorage.setItem(
-                "auth",
-                JSON.stringify({ 
-                    access, refresh: getRefreshToken(), user: getUser() }));
-            })
-        .catch(() => {
-            localStorage.removeItem("auth");});
+  return axios
+    .post(
+      "/auth/refresh/",
+      {
+        refresh: getRefreshToken(),
+      },
+      {
+        baseURL: "ec2-16-170-201-40.eu-north-1.compute.amazonaws.com",
+      }
+    )
+    .then((resp) => {
+      const { access } = resp.data;
+      failedRequest.response.config.headers["Authorization"] =
+        "Bearer " + access;
+      localStorage.setItem(
+        "auth",
+        JSON.stringify({ access, refresh: getRefreshToken(), user: getUser() })
+      );
+    })
+    .catch(() => {
+      localStorage.removeItem("auth");
+    });
 };
-
 
 createAuthRefreshInterceptor(axiosService, refreshAuthLogic);
 
 export function fetcher(url) {
-    return axiosService.get(url).then((res) => res.data);
+  return axiosService.get(url).then((res) => res.data);
 }
 
-
 export default axiosService;
-
