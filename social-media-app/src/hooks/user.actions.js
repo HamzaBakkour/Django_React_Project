114e121d26a67dfa2f3fba1a202282a68b428a5c
HamzaBakkour <alexander.x4.hb@outlook.com>
2023-08-21 00:44:25 +0200

    m-c

--- social-media-app/src/hooks/user.actions.js
+++ social-media-app/src/hooks/user.actions.js
@@ -1,96 +1,106 @@
-//src/hooks/user.actions.js
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import axtiosService from "../helpers/axios";
+import axiosService from "../helpers/axios";
+import axios from "axios";
 
 function useUserActions() {
-    const navigate = useNavigate();
-    const baseURL = "ec2-16-170-201-40.eu-north-1.compute.amazonaws.com";
+  const navigate = useNavigate();
+  const baseURL = "ec2-16-170-201-40.eu-north-1.compute.amazonaws.com";
 
- 
-    return {
-        login,
-        register,
-        logout,
-        edit
-    };
+  return {
+    login,
+    register,
+    logout,
+    edit,
+  };
 
-    // Login the user
-    function login(data) {
-            return axios.post(`${baseURL}/auth/login/`, data).then((res) => {
-            // Registering the account and tokens in the store
-            setUserData(res.data);
-            navigate("/");
-        });
-    }
+  // Login the user
+  function login(data) {
+    return axios.post(`${baseURL}/auth/login/`, data).then((res) => {
+      // Registering the account and tokens in the store
+      setUserData(res.data);
+      navigate("/");
+    });
+  }
 
-    // Register the user
-    function register(data) {
-            return axios.post(`${baseURL}/auth/register/`, data).then((res) => {
-            // Registering the account and tokens in the store
-            setUserData(res.data);
-            navigate("/");
-        });
-    }
+  // Register the user
+  function register(data) {
+    return axios.post(`${baseURL}/auth/register/`, data).then((res) => {
+      // Registering the account and tokens in the store
+      setUserData(res.data);
+      navigate("/");
+    });
+  }
+
+  // Edit the user
+  function edit(data, userId) {
+    return axiosService
+      .patch(`${baseURL}/user/${userId}/`, data, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        // Registering the account in the store
+        localStorage.setItem(
+          "auth",
+          JSON.stringify({
+            access: getAccessToken(),
+            refresh: getRefreshToken(),
+            user: res.data,
+          })
+        );
+      });
+  }
 
   // Logout the user
-    function logout() {
+  function logout() {
+    return axiosService
+      .post(`${baseURL}/auth/logout/`, { refresh: getRefreshToken() })
+      .then(() => {
         localStorage.removeItem("auth");
         navigate("/login");
-    }
-
-    function edit(data, userId) {
-        return axtiosService.patch(`${baseURL}/user/${userId}/`, data, {
-            headers: {
-                "content-type": "multipart/form-data",
-                },
-            })
-            .then((res) => {
-                localStorage.setItem(
-                                    "auth",
-                                    JSON.stringify({
-                                        access: getAccessToken(),
-                                        refresh: getRefreshToken(),
-                                        user: res.data,
-                                    })
-                            );
-            });
-        }
+      });
+  }
 }
 
 // Get the user
 function getUser() {
-    const auth = JSON.parse(localStorage.getItem("auth")) || null;
-    if (auth) {
-        return auth.user;
-    } else {
-        return null;
-    }
+  const auth = JSON.parse(localStorage.getItem("auth")) || null;
+  if (auth) {
+    return auth.user;
+  } else {
+    return null;
+  }
 }
 
 // Get the access token
 function getAccessToken() {
-    const auth = JSON.parse(localStorage.getItem("auth"));
-    return auth.access;
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  return auth.access;
 }
 
 // Get the refresh token
 function getRefreshToken() {
-    const auth = JSON.parse(localStorage.getItem("auth"));
-    return auth.refresh;
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  return auth.refresh;
 }
 
 // Set the access, token and user property
 function setUserData(data) {
-    localStorage.setItem(
-        "auth",
-        JSON.stringify({
-            access: data.access,
-            refresh: data.refresh,
-            user: data.user,
-        })
-    );
+  localStorage.setItem(
+    "auth",
+    JSON.stringify({
+      access: data.access,
+      refresh: data.refresh,
+      user: data.user,
+    })
+  );
 }
 
-export { useUserActions, getUser, getAccessToken, getRefreshToken };
-
+export {
+  useUserActions,
+  getUser,
+  getAccessToken,
+  getRefreshToken,
+  setUserData,
+};
