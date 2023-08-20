f8fa1011e65549e6c16af32d94f6803fc783ac17
HamzaBakkour <alexander.x4.hb@outlook.com>
2023-08-20 21:09:23 +0200

    m-c

--- social-media-app/src/index.js
+++ social-media-app/src/index.js
@@ -11,10 +11,10 @@ const root = ReactDom.createRoot(
   document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
